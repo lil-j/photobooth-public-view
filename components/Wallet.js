1c8a0e8f8ec7e9fca26b72f0172cfd357639bc61
lil-j <jake@krunchy.net>
2024-10-05 21:09:51 -0700

    wallet updates

--- components/Wallet.js
+++ components/Wallet.js
@@ -3,8 +3,10 @@
 import Image from "next/image";
 import AppleWallet from "@/app/apple_wallet.webp";
 import {motion} from "framer-motion";
+import {useState} from "react";
 
 export default function AddToWallet({id}) {
+    const [loading, setLoading] = useState(false)
     return (
         <motion.div
             initial={{ y: 100 }}
@@ -24,12 +26,23 @@ export default function AddToWallet({id}) {
         >
             <div
                 onClick={() => {
-                    window.open(`/api/get-pass?strip_id=${id}`, "_blank");
+                    setLoading(true)
+                    window.open(`/api/get-pass?strip_id=${id}`, "_self")
+                    setTimeout(
+                        () => {
+                            setLoading(false)
+                        }
+                ,
+                        3000
+                    )
                 }}
                 className="cursor-pointer items-center border-2 border-black gap-2 px-8 py-2 text-white font-bold whitespace-nowrap flex bg-black/70 backdrop-blur tracking-tight rounded-xl"
             >
                 <Image src={AppleWallet} alt="Apple Wallet Logo" className="w-10 h-10" />
-                <p>Add to your Apple Wallet</p>
+                <p>{
+                    loading ? "Adding to Wallet..." : "Add to your Apple Wallet"
+
+                }</p>
             </div>
         </motion.div>
     );
